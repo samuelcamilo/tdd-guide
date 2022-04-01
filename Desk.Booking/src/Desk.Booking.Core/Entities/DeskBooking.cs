9212aff205d3a5575102c80cdc07abb704e8cfa0
Samuel Camilo <scamilo.dev@gmail.com>
2022-04-01 01:19:44 -0300

    Components oriented on abstrations

---
+++ Desk.Booking/src/Desk.Booking.Core/Entities/DeskBooking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desk.Booking.Core.Entities
+{
+    public class DeskBooking
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public DateTime DateRequest { get; set; }
+    }
+}
---
+++ Desk.Booking/src/Desk.Booking.Core/IDeskBookingData.cs
@@ -0,0 +1,9 @@
+﻿using Desk.Booking.Core.Entities;
+
+namespace Desk.Booking.Core
+{
+    public interface IDeskBookingData
+    {
+        void Save(DeskBooking deskBooking);
+    }
+}
